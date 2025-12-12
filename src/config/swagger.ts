8a4b925aff74f3a32fefb5cff8633bcdba832175
serrabruna <brunaibserra@hotmail.com>
2025-12-11 23:55:58 -0300

    configurando swagger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -6,6 +6,27 @@ import fs from 'fs';
 const swaggerFile = path.join(__dirname, '../swagger.json');
 const swaggerSpec = JSON.parse(fs.readFileSync(swaggerFile, 'utf8'));
 
+const swaggerOptions = {
+    swaggerOptions: {
+        url: '/swagger.json', 
+        persistAuthorization: true,
+        displayRequestDuration: true,
+        filter: true,
+        syntaxHighlight: {
+            activate: true,
+            theme: 'monokai'
+        }
+    }
+};
+
 export const setupSwagger = (app: Express) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-}
+    app.get('/swagger.json', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec);
+    });
+
+    app.use('/api-docs', swaggerUi.serve);
+    app.get('/api-docs', swaggerUi.setup(swaggerSpec, swaggerOptions));
+    
+    console.log('Swagger configurado em /api-docs');
+};
